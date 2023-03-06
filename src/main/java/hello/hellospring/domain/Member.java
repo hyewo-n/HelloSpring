20f5ed922da5bbc0629740acd55e8fdd786a782c
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2023-03-06 23:26:47 +0900

    스프링 DB 접근_JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,18 @@
 package hello.hellospring.domain;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
     // 요구사항
+
+    // DB가 알아서 생성해주는 것 = IDENTITY
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;       // 데이터를 구분하기 위해서 시스템이 값을 정함
     private String name;   // 회원가입 시 고객이 입력하는 값
 
--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,22 +1,29 @@
 package hello.hellospring.service;
 
-import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
-
+    /*
     private DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+     */
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     // Spring Bean에 입력하라는 뜻이네! 하고 로직을 인식하여 등록을 해줌
@@ -29,7 +36,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        // return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
